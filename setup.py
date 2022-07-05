6650b84b7399b44c96530434ee8c25b6a09982f5
thelicato <thelicato@users.noreply.github.com>
2022-07-05 14:55:42 +0200

    Fix minor bug

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@
 from setuptools import find_packages, setup
 from pathlib import Path
 
-def read(rel_path):
+def read(rel_path, fix_header=False):
     init = Path(__file__).resolve().parent / rel_path
     initial_content = init.read_text('utf-8', 'ignore')
-    fixed_header = '<h1 align="center"><img src="https://raw.githubusercontent.com/cybersecsi/nmapvulners2csv/main/assets/logo-light-mode.png" alt= "nmapvulners2csv" width="300px"></h1>'
-    content = fixed_header +  initial_content.split('</h1>')[1]
-    return content
+    if fix_header:
+        fixed_header = '<h1 align="center"><img src="https://raw.githubusercontent.com/cybersecsi/nmapvulners2csv/main/assets/logo-light-mode.png" alt= "nmapvulners2csv" width="300px"></h1>'
+        content = fixed_header + initial_content.split('</h1>')[1]
+        return content
+    else:
+        return initial_content
 
 def get_version():
     ver_path = 'nmapvulners2csv/nmapvulners2csv.py'
@@ -40,7 +43,7 @@ setup(
         ],
     },
     url='https:/github.com/cybersecsi/nmapvulners2csv',
-    long_description=read('README.md'),
+    long_description=read('README.md', fix_header=True),
     long_description_content_type='text/markdown',
     install_requires=Path('requirements.txt').read_text().splitlines(),
 )
